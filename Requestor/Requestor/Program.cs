1ae0568e84d08f38b7c20595e24afd18bc9ad080
Brian Sullivan <bmsullivan@gmail.com>
2012-08-26 13:51:40 -0500

    Add actual code

---
+++ Requestor/Requestor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Requestor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startTime = DateTime.Now;            
+            var tasks = new List<Task>();
+            for(int i = 0; i < 100; i++)
+            {
+                var client = new WebClient();
+                tasks.Add(Task.Factory.StartNew(() => client.DownloadData(new Uri("http://localhost/MVCAsync/Home/WidgetsAsync"))));    
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            Console.WriteLine("Time elapsed: " + DateTime.Now.Subtract(startTime));
+        }
+    }
+}
